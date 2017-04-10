704d886b5a22e8b1ad6d39918b1eb8bf5555250c
medeotl <suninsland@yahoo.it>
2017-04-10 16:54:02 +0200

    rinomino   con --> conn    e    cur --> c

--- PROVE/provaDB/3esempio.py
+++ PROVE/provaDB/3esempio.py
@@ -4,20 +4,20 @@
 
 import sqlite3 as lite
 
-con = lite.connect('test.db')
+conn = lite.connect('test.db')
 
-with con:
+with conn:
 
-    cur = con.cursor()
-    cur.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
-    cur.execute("INSERT INTO Cars VALUES(1, 'Audi', 52642)")
-    cur.execute("INSERT INTO Cars VALUES(2, 'Mercedes', 57127)")
-    cur.execute("INSERT INTO Cars VALUES(3, 'Skoda', 9000)")
-    cur.execute("INSERT INTO Cars VALUES(4, 'Volvo', 29000)")
-    cur.execute("INSERT INTO Cars VALUES(5, 'Bentley', 350000)")
-    cur.execute("INSERT INTO Cars VALUES(6, 'Citroen', 21000)")
-    cur.execute("INSERT INTO Cars VALUES(7, 'Hummer', 41400)")
-    cur.execute("INSERT INTO Cars VALUES(8, 'Volkswagen', 21600)")
+    c = conn.cursor()
+    c.execute("CREATE TABLE Cars(Id INT, Name TEXT, Price INT)")
+    c.execute("INSERT INTO Cars VALUES(1, 'Audi', 52642)")
+    c.execute("INSERT INTO Cars VALUES(2, 'Mercedes', 57127)")
+    c.execute("INSERT INTO Cars VALUES(3, 'Skoda', 9000)")
+    c.execute("INSERT INTO Cars VALUES(4, 'Volvo', 29000)")
+    c.execute("INSERT INTO Cars VALUES(5, 'Bentley', 350000)")
+    c.execute("INSERT INTO Cars VALUES(6, 'Citroen', 21000)")
+    c.execute("INSERT INTO Cars VALUES(7, 'Hummer', 41400)")
+    c.execute("INSERT INTO Cars VALUES(8, 'Volkswagen', 21600)")
 
 # Nota: Using the with keyword the changes are automatically committed. 
-#       Otherwise, we would have to commit them manually ( con.commit() )
+#       Otherwise, we would have to commit them manually ( conn.commit() )

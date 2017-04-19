b6b735d5aac3dac9ef650710b0de70c545b10f77
medeotl <suninsland@yahoo.it>
2017-04-19 15:27:07 +0200

    sistemato esempio6

--- PROVE/provaDB/6esempio.py
+++ PROVE/provaDB/6esempio.py
@@ -7,19 +7,19 @@
 
 import sqlite3
 
-conn = sqlite3.connect(':memory:') #
+conn = sqlite3.connect(':memory:')
 
 with conn:
 
     c = conn.cursor()
     c.execute("CREATE TABLE Friends(Id INTEGER PRIMARY KEY, Name TEXT);")
     c.execute("INSERT INTO Friends(Name) VALUES ('Tom');")
-    c.execute("INSERT INTO Friends(Name) VALUES ('Rebecca')") # no ;
+    c.execute("INSERT INTO Friends(Name) VALUES ('Rebecca')") # niente ;
     c.execute("INSERT INTO Friends(Name) VALUES ('Jim');")
     c.execute("INSERT INTO Friends(Name) VALUES ('Robert');")
 
     lid = c.lastrowid
-    print "The last Id of the inserted row is %d" % lid
+    print ("The last Id of the inserted row is %d" % lid)
 
 # Nota: in questo esercizio l'autore ha messo il ";" finale alle query SQL
-#       può anche essere omessa (vedi linea 16)
+#       può anche essere omessa (vedi linea 17)

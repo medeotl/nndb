d4de4d3e2e1f49ed1060c81f236cebad796d2b29
medeotl <suninsland@yahoo.it>
2017-04-19 15:42:30 +0200

    modifiche minori

--- PROVE/provaDB/7retriveData.py
+++ PROVE/provaDB/7retriveData.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python3
 # -*- coding: utf-8 -*-
 #
-# primo esempio di prelevamento dati dal db
+# primo esempio di estrazione dati dal db
 # uso di c.fetchall() che ritorna una tupla di tuple
 
 import sqlite3
@@ -16,4 +16,4 @@ with conn:
     rows = c.fetchall()
 
     for row in rows:
-        print row
+        print (row)
